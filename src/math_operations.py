7e0cc5d69898e15834a75eeb6891b7119d6bc405
rahimnadan <rahimwazir166@gmail.com>
2025-07-16 10:25:27 +0500

    Add new functions and their tests

--- src/math_operations.py
+++ src/math_operations.py
@@ -3,3 +3,10 @@ def add(a,b):
 
 def sub(a,b):
     return a-b 
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,4 +1,4 @@
-from src.math_operations import add,sub
+from src.math_operations import add,sub,mul,div
 
 def test_add():
     assert add(2,3)==5
@@ -9,3 +9,16 @@ def test_sub():
     assert sub(4,3)==1
     assert sub(3,3)==0
     assert sub(2,3)==-1
+
+def test_mul():
+    assert mul(2,3)==6
+    assert mul(-1,1)==-1
+    assert mul(0,0)==0
+
+def test_div():
+    assert div(6,3)==2
+    assert div(-1,1)==-1
+    assert div(0,1)==0
+    assert div(1,0)==0
+
+
